9bebf0856c28c43a4f908486c1ff37a433e053a9
Tri Nanda <2001020024@student.umrah.ac.id>
2024-11-04 13:35:43 +0700

    Membuat chart

---
+++ app/Filament/Widgets/WidgetExpendsChart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\Transaction;
+
+
+class WidgetExpendsChart extends ChartWidget
+{
+    protected static ?string $heading = 'Pengeluaran';
+    protected static string $color = 'danger';
+
+    protected function getData(): array
+    {
+        $data = Trend::query(Transaction::expenses())
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perDay()
+            ->sum('amount');
+    
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
